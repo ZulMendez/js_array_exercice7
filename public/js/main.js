1589026e9c56c47b01735327eb266597eabbbe4f
zulma <zumendez7@gmail.com>
2021-01-10 14:07:43 +0100

    end

---
+++ public/js/main.js
@@ -0,0 +1,51 @@
+// exo1
+
+let classe = ['Bene', 'Mohamed', 'Ayhan', 'Zulma'];
+console.log(classe);
+
+console.log(classe.indexOf('Zulma'));
+console.log(classe.length);
+
+// exo2
+let lesplusbelles = ['Zulma'];
+console.log(lesplusbelles);
+
+lesplusbelles.push('Bene');
+console.log(lesplusbelles);
+
+// exo3
+let classeCoding16 = [];
+
+let coach = classeCoding16.push('Ayhan');
+
+let premier = classeCoding16.push('Benedicte');
+
+let deux = classeCoding16.push('Louise', 'Jamila');
+
+let trois = classeCoding16.push('Sebastien', 'Adame');
+
+let quatr = classeCoding16.push('Dawid', 'Malo');
+
+let cinq = classeCoding16.push('Alex');
+// classeCoding16.pop();
+
+let six = classeCoding16.push('Maxence', 'Mohammed');
+
+let sept = classeCoding16.push('Saliou');
+
+let huit = classeCoding16.push('Zulma', 'Maximilien');
+
+let neuf = classeCoding16.push('Natchez');
+
+let dix = classeCoding16.push('Zakaria', 'Ayoub');
+
+// classeCoding16.shift();
+
+let once = classeCoding16.push('Achraf');
+
+let douze = classeCoding16.push('Abdellah');
+
+console.log(classeCoding16);
+
+console.log(`${classeCoding16[0]} arrive en classe, ${classeCoding16[1]} rentre, allume son PC et commence à mettre à jour ses notes
+${classeCoding16[2]} et ${classeCoding16[3]} rejoignent la classe. ${classeCoding16[4]} et ${classeCoding16[5]} voyent Ayhan passer devant la porte, et décident de le rejoindre, ${classeCoding16[6]} et ${classeCoding16[7]} rentrent en classe en faisant un maximum de bruit ${classeCoding16[8]} rentre en classe puis redescend prendre sa tasse de café, ${classeCoding16[9]} et ${classeCoding16[10]} rentre en classe saluant tout le monde, ${classeCoding16[11]} rentre tranquil et observe tout le monde. ${classeCoding16[12]} et ${classeCoding16[13]} rentrent et observent Jamila qui à l'air d'être tranquilleeee... ${classeCoding16[14]} rentre en classe avec sa tasse de café; ${classeCoding16[15]} et ${classeCoding16[16]} rentrent en classe tous contents de n'être que 5 minutes en retard, ${classeCoding16[0]} leur dit qu'ils sont en retard et puis il sort. ${classeCoding16[17]} rentre avec sa petite tasse de café et son pc, tout content et souriant. ${classeCoding16[18]} rentre</b> en faisant le minimum de bruit possible mais toute la classe la regarde.`)
